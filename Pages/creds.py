3b5e64f7743b9b0352e9a8230252c4faf4650d82
Sami <remedy@Remedy.local>
2024-08-06 11:55:22 +0300

    a full feature automated python program

---
+++ Pages/creds.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+username='test@test.com'
+password='12345678'
+
+email='//*[@id="email"]'
---
+++ Testcases/test_XpertbotLogin.py
@@ -0,0 +1,30 @@
+import pytest
+
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException , TimeoutException
+from Pages.creds import *
+
+from Pages.XpertbotLogin import Xpert
+from Utilities.utils import AutomationLogger
+
+@pytest.mark.usefixtures("LunchDriver")
+class TestXperbotlogin:
+    log= AutomationLogger.automation()
+    test_date=AutomationLogger.get_newest_excel_file("/Users/remedy/Documents/pytest/Pytest/Testdata","Sheet1")
+    @pytest.mark.parametrize("test_data",test_date)
+    def test_xpertbotlogin(self,test_data):
+        try:    
+            self.log.info("Test Started")
+            X=Xpert(self.driver)
+            self.log.info("Sent Driver")
+            X.Xpertbottesting(**test_data)
+            self.log.info("Sent Data")
+        except Exception as e:
+            self.log.Error(f"Test Failed {e}")
+            pytest.fail()
